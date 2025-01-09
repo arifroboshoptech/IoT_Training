3ea910c6841846793b6edb446b1f6366e3e187e6
Muhammad Arif Ridhwan Bin Mohamed Jalil <74816249+arifroboshoptech@users.noreply.github.com>
2025-01-09 18:48:26 +0800

    Add files via upload

---
+++ ESP8266_MQTT/ESP8266_MQTT.ino
@@ -0,0 +1,177 @@
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include "DHT.h"
+
+// Uncomment one of the lines bellow for whatever DHT sensor type you're using!
+#define DHTTYPE DHT11   // DHT 11
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+//#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+
+// Change the credentials below, so your ESP8266 connects to your router
+const char* ssid = "REPLACE_WITH_YOUR_SSID";
+const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+
+// MQTT broker credentials (set to NULL if not required)
+const char* MQTT_username = NULL; 
+const char* MQTT_password = NULL;
+
+// Change the variable to your Raspberry Pi IP address, so it connects to your MQTT broker
+#define mqtt_server "broker.hivemq.com"
+#define mqtt_port 1883
+
+//Publish MQTT Topics
+#define MQTT_PUB_TEMP "room/temperature"
+#define MQTT_PUB_HUM "room/humidity"
+
+//Subscribe MQTT Topics
+#define MQTT_SUB_LAMP1 "room/lamp"
+
+//Timer to send data
+#define PUSH_TIMER 30000
+
+
+// Initializes the espClient. You should change the espClient name if you have multiple ESPs running in your home automation system
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+// DHT Sensor - GPIO 5 = D1 on ESP-12E NodeMCU board
+const int DHTPin = 5;
+
+// Lamp - LED - GPIO 4 = D2 on ESP-12E NodeMCU board
+const int lamp = 4;
+
+// Initialize DHT sensor.
+DHT dht(DHTPin, DHTTYPE);
+
+// Timers auxiliar variables
+long now = millis();
+long lastMeasure = 0;
+
+// This functions connects your ESP8266 to your router
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("WiFi connected - ESP IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+// This function is executed when some device publishes a message to a topic that your ESP8266 is subscribed to
+// Change the function below to add logic to your program, so when a device publishes a message to a topic that 
+// your ESP8266 is subscribed you can actually do something
+void callback(String topic, byte* message, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+  
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
+
+
+  // If a message is received on the topic room/lamp, you check if the message is either on or off. Turns the lamp GPIO according to the message
+  if(topic == MQTT_SUB_LAMP1){
+      Serial.print("Changing Room lamp to ");
+      if(messageTemp == "on"){
+        digitalWrite(lamp, HIGH);
+        Serial.print("On");
+      }
+      else if(messageTemp == "off"){
+        digitalWrite(lamp, LOW);
+        Serial.print("Off");
+      }
+  }
+  Serial.println();
+}
+
+// This functions reconnects your ESP8266 to your MQTT broker
+// Change the function below if you want to subscribe to more topics with your ESP8266 
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+
+    if (client.connect("ESP8266Client", MQTT_username, MQTT_password)) {
+      Serial.println("connected");  
+      // Subscribe or resubscribe to a topic
+      // You can subscribe to more topics (to control more LEDs in this example)
+      client.subscribe(MQTT_SUB_LAMP1);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+// The setup function sets your ESP GPIOs to Outputs, starts the serial communication at a baud rate of 115200
+// Sets your mqtt broker and sets the callback function
+// The callback function is what receives messages and actually controls the LEDs
+void setup() {
+  pinMode(lamp, OUTPUT);
+  
+  dht.begin();
+  
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+
+}
+
+// For this project, you don't need to change anything in the loop function. Basically it ensures that you ESP is connected to your broker
+void loop() {
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  if(!client.loop())
+    client.connect("ESP8266Client", MQTT_username, MQTT_password);
+
+  now = millis();
+  // Publishes new temperature and humidity every 30 seconds
+  if (now - lastMeasure > PUSH_TIMER) {
+    lastMeasure = now;
+    // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+    float humidity = dht.readHumidity();
+    // Read temperature as Celsius (the default)
+    float temperatureC = dht.readTemperature();
+    // Read temperature as Fahrenheit (isFahrenheit = true)
+    float temperatureF = dht.readTemperature(true);
+
+    // Check if any reads failed and exit early (to try again).
+    if (isnan(humidity) || isnan(temperatureC) || isnan(temperatureF)) {
+      Serial.println("Failed to read from DHT sensor!");
+      return;
+    }
+
+    // Publishes Temperature and Humidity values
+    client.publish(MQTT_PUB_TEMP, String(temperatureC).c_str());
+    client.publish(MQTT_PUB_HUM, String(humidity).c_str());
+    //Uncomment to publish temperature in F degrees
+    //client.publish("room/temperature", String(temperatureF).c_str());
+    
+    Serial.print("Humidity: ");
+    Serial.print(humidity);
+    Serial.println(" %");
+    Serial.print("Temperature: ");
+    Serial.print(temperatureC);
+    Serial.println(" ºC");
+    Serial.print(temperatureF);
+    Serial.println(" ºF");
+  }
+} 
